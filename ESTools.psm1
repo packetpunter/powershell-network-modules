b5ef95ec50a636a03b89767c003c8b57ec88ed0a
John Bell <packetpunter@gmail.com>
2021-03-29 16:16:38 -0600

    steps in a direction

--- ESTools.psm1
+++ ESTools.psm1
@@ -11,32 +11,45 @@ function Invoke-ElasticMessage () {
 
         [Parameter(
             Mandatory=$True,
-            HelpMessage="The message to post in a key,value pair. e.g. @('Name','TheBest')",
+            HelpMessage="The index in elastic to store the message in. This is used to construct the URL endpoint for post/get.",
             Position=1
         )]
         [ValidateNotNullOrEmpty()]
-        [System.Array] $MessageKVPair,
+        [String] $ElasticIndex,
 
         [Parameter(
             Mandatory=$False,
-            HelpMessage="Flag if this uses SSL or not",
-            Position=2
+            HelpMessage="Flag if this uses SSL or not. This is used to construct the URL endpoint."
         )]
-        [Switch] $SSLEnabled
+        [Switch] $SSLEnabled,
 
+        [Parameter( 
+            Mandatory=$True,
+            HelpMessage="The data to store."
+        )]
+        [ValidateNotNullOrEmpty()]
+        [String] $Data,
+
+        [Parameter(
+            Mandatory=$False,
+            HelpMessage="The URL source of the data in the message."
+        )]
+        [ValidateNotNullOrEmpty()]
+        [String] $Source
     )
 
     Begin {
         if ($SSLEnabled){
-            $URI = "https://" + $ServerConnection + "/"
+            $URI = "https://" + $ServerConnection + "/" + $ElasticIndex + "/_doc " + $id; 
         }else{
-            $URI = "http://" + $ServerConnection + "/"
+            $URI = "http://" + $ServerConnection + "/"+ $ElasticIndex + "/_doc " + $id;
         }
         
     }
     Process {
         Invoke-UserMessage -MessageToUser "Connection to $URI pending.." -Level Success
-        Invoke-UserMessage -MessageToUser "Will post $MessageKVPair" -Level Standard
+        Invoke-UserMessage -MessageToUser "Will post $Data in the $ElasticIndex index" -Level Standard
+        #Invoke-RestMethod -Method Put -Uri $URI -Body $Data
 
     }
     End{
