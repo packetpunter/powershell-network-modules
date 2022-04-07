37fe8c27e5c25d3fa1efaf4e68ec2d378826d320
John Bell <packetpunter@gmail.com>
2022-04-06 20:11:52 -0600

    added error checking

--- bgpview_query.ps1
+++ bgpview_query.ps1
@@ -1,12 +1,30 @@
-﻿function Get-SplunkASNPrefixSearch() {
+﻿class ValidateASN: System.Management.Automation.ValidateArgumentsAttribute {
+    # https://en.wikipedia.org/wiki/Autonomous_system_(Internet) for list of valid ASN ranges
+    [void] Validate ([object]$arguments, [System.Management.Automation.EngineIntrinsics]$engineIntrinsics){
+        switch([Int] $arguments) {
+            {$PSItem -le 1}{ Throw [System.ArgumentException]::new() }
+            {$PSItem -ge 64496 -and $PSItem -le 131072}{Throw [System.ArgumentException]::new()}
+            {$PSItem -gt 4199999999 }{Throw [System.ArgumentException]::new()}   
+        }
+    }
+}
+
+function Get-SplunkASNPrefixSearch() {
     Param(
         [Parameter(
             Mandatory=$True,
             HelpMessage="Autonomous System Number to find Prefixes for",
             Position=0
         )]
+        [ValidateASN()]
+        [String] $ASN,
+
+        [Parameter(
+            Mandatory=$False,
+            HelpMessage="Outfile path for query results in CSV"
+        )]
         [ValidateNotNullOrEmpty()]
-        [String] $ASN
+        [String] $CSVToFile
     )
     Begin {
         $BGPViewURL = "https://api.bgpview.io/asn/{}/prefixes"
@@ -18,23 +36,24 @@
     Process{
         if ($Confirmation -eq 'Y') {
             $URL = $BGPViewURL -replace "{}",$ASN
-            $Data = iwr -Uri $URL -UseBasicParsing -Method Get | ConvertFrom-Json
+            $Data = Invoke-WebRequest -Uri $URL -UseBasicParsing -Method Get | ConvertFrom-Json
             foreach($Prefix_set in $Data.data.ipv4_prefixes) {
                 $Prefixes = $Prefixes + $Prefix_set.prefix
             }
         }else{
             Write-Host "Cancelled"
+            Break
         }
     }
     End{
-        Write-Host "End.. prefixes:" $Prefixes.Length
+        Write-Verbose "# Found prefixes: {}".replace("{}", $Prefixes.Length)
         $SPL_FULL = 'index="networking" (xXx) | eval destProto=transport+"/"+dest_port| stats count by src_ip, dest_ip, destProto, vendor_action, session_end_reason'
         $Tuple = '(src_ip="{}" OR dest_ip="{}") OR'
         $TupleFin = '(src_ip="{}" OR dest_ip="{}")'
         $Tups = @()
         for ($i = 0; $i -lt $Prefixes.Length; $i++){
             $p = $Prefixes[$i]
-            Write-Host "Processing prefix" $p
+            Write-Verbose "Processing prefix {}".replace("{}", $p)
             if($i -ne $Prefixes.Length-1){
                 $TempTupl = $Tuple -replace "{}",$p
                 $Tups += $TempTupl
