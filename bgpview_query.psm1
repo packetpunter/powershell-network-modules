ba56669f261181139bdfa80a8e0ad5a72aec79ab
John Bell <packetpunter@gmail.com>
2022-04-07 08:53:47 -0600

    separated functionality into new cmdlets

--- bgpview_query.psm1
+++ bgpview_query.psm1
@@ -9,6 +9,92 @@
     }
 }
 
+function Invoke-BGPViewASNQuery() {
+    Param(
+        [Parameter(
+            Mandatory=$True,
+            HelpMessage="Autonomous System Number to find Prefixes for",
+            Position=0
+        )]
+        [ValidateASN()]
+        [String] $ASN,
+        
+        [Parameter(
+            Mandatory=$False,
+            HelpMessage="Outfile path for query results in CSV"
+        )]
+        [ValidateNotNullOrEmpty()]
+        [String] $CSVToFile
+    )
+    Begin{
+        $BGPViewURL = "https://api.bgpview.io/asn/{}/prefixes"
+        $QConfirm = "Query for AS#"+$ASN+"? (Y/N)"
+        $Confirmation = Read-Host $QConfirm
+        $Prefixes = @()
+    }
+    Process{
+        if ($Confirmation -eq 'Y') {
+            $URL = $BGPViewURL -replace "{}",$ASN
+            $Data = Invoke-WebRequest -Uri $URL -UseBasicParsing -Method Get | ConvertFrom-Json
+            foreach($Prefix_set in $Data.data.ipv4_prefixes) {
+                $Prefixes = $Prefixes + $Prefix_set.prefix
+            }
+        }else{
+            Write-Host "Cancelled"
+            Break
+        }
+    }
+    End{
+        Write-Verbose "# Found prefixes: {}".replace("{}", $Prefixes.Length)
+        if ($CSVToFile) {
+            $OutputData = @()
+            $date = Get-Date -Format "MM-dd-yyyyTHH:mm:ss"
+            Write-Verbose "CIDR`tASN`tTimeStamp:"
+            foreach($cidr in $Prefixes){
+                Write-Verbose "x`ty`tz".replace("x",$cidr).replace("y",$ASN).replace("z", $date)
+                $Entry = New-Object psobject
+                $Entry | Add-Member -MemberType NoteProperty -Name "CIDR" -Value $cidr
+                $Entry | Add-Member -MemberType NoteProperty -Name "ASN" -Value $ASN
+                $Entry | Add-Member -MemberType NoteProperty -Name "Timestamp" -Value $date
+                $OutputData += $Entry
+            }
+            $OutputData | Export-CSV -Path $CSVToFile -Append
+        }
+        return $Prefixes
+    }
+}
+
+
+function Get-SPLForSubnets() {
+    Param(
+        [Parameter(
+            Mandatory=$True,
+            HelpMessage="The array of prefixes to generate SPL search for.",
+            Position=0
+        )]
+        [ValidateNotNullOrEmpty()]
+        [String[]] $PrefixList 
+    )
+
+    $SPL_FULL = 'index="networking" (xXx) | eval destProto=transport+"/"+dest_port| stats count by src_ip, dest_ip, destProto, vendor_action, session_end_reason'
+    $SPL_SrcDest = '(src_ip="{}" OR dest_ip="{}") OR'
+    $SPL_SrcDestFin = '(src_ip="{}" OR dest_ip="{}")'
+    $Terms = @()
+    for ($i = 0; $i -lt $PrefixList.Length; $i++){
+        $p = $PrefixList[$i]
+        Write-Verbose "Processing prefix {}".replace("{}", $p)
+        if($i -ne $PrefixList.Length-1){
+            $Temp = $SPL_SrcDest -replace "{}",$p
+            $Terms += $Temp
+        }else{
+            $Temp = $SPL_SrcDestFin -replace "{}",$p
+            $Terms += $Temp
+        }
+    }
+    $FinalFull = $SPL_FULL -replace "xXx",$Terms
+    Write-Host `n`n$FinalFull`n`n
+}
+
 function Get-SplunkASNPrefixSearch() {
     Param(
         [Parameter(
@@ -26,61 +112,14 @@ function Get-SplunkASNPrefixSearch() {
         [ValidateNotNullOrEmpty()]
         [String] $CSVToFile
     )
-    Begin {
-        $BGPViewURL = "https://api.bgpview.io/asn/{}/prefixes"
-        $QConfirm = "Query for AS#"+$ASN+"? (Y/N)"
-        $Confirmation = Read-Host $QConfirm
-        $Prefixes = @()
-    }
-    
     Process{
-        if ($Confirmation -eq 'Y') {
-            $URL = $BGPViewURL -replace "{}",$ASN
-            $Data = Invoke-WebRequest -Uri $URL -UseBasicParsing -Method Get | ConvertFrom-Json
-            foreach($Prefix_set in $Data.data.ipv4_prefixes) {
-                $Prefixes = $Prefixes + $Prefix_set.prefix
-            }
-        }else{
-            Write-Host "Cancelled"
-            Break
-        }
+        $subnets = Invoke-BGPViewASNQuery -ASN $ASN
     }
     End{
-        Write-Verbose "# Found prefixes: {}".replace("{}", $Prefixes.Length)
-        $SPL_FULL = 'index="networking" (xXx) | eval destProto=transport+"/"+dest_port| stats count by src_ip, dest_ip, destProto, vendor_action, session_end_reason'
-        $Tuple = '(src_ip="{}" OR dest_ip="{}") OR'
-        $TupleFin = '(src_ip="{}" OR dest_ip="{}")'
-        $Tups = @()
-        for ($i = 0; $i -lt $Prefixes.Length; $i++){
-            $p = $Prefixes[$i]
-            Write-Verbose "Processing prefix {}".replace("{}", $p)
-            if($i -ne $Prefixes.Length-1){
-                $TempTupl = $Tuple -replace "{}",$p
-                $Tups += $TempTupl
-            }else{
-                $TempTupleFin = $TupleFin -replace "{}",$p
-                $Tups += $TempTupleFin
-            }
-        }
-
-        $FinalFull = $SPL_FULL -replace "xXx",$Tups
-        Write-Host `n`n$FinalFull`n`n
-        
-        if ($CSVToFile) {
-            $OutputData = @()
-            $date = Get-Date -Format "MM-dd-yyyyTHH:mm:ss"
-            Write-Verbose "CIDR`tASN`tTimeStamp:"
-            foreach($cidr in $Prefixes){
-                Write-Verbose "x`ty`tz".replace("x",$cidr).replace("y",$ASN).replace("z", $date)
-                $Entry = New-Object psobject
-                $Entry | Add-Member -MemberType NoteProperty -Name "CIDR" -Value $cidr
-                $Entry | Add-Member -MemberType NoteProperty -Name "ASN" -Value $ASN
-                $Entry | Add-Member -MemberType NoteProperty -Name "Timestamp" -Value $date
-                $OutputData += $Entry
-            }
-            $OutputData | Export-CSV -Path $CSVToFile -Append
-        }
+        Get-SPLForSubnets $subnets
     }
 }
 
 Export-ModuleMember Get-SplunkASNPrefixSearch
+Export-ModuleMember Get-SPLForSubnets
+Export-ModuleMember Invoke-BGPViewASNQuery
